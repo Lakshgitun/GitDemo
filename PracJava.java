0d4ffe02f8ad886480d543346c271d28f98ae715
Lakshgitun <lakshmipathi2290@gmail.com>
2022-02-07 09:31:13 +0530

    original

---
+++ PracJava.java
@@ -0,0 +1,18 @@
+package javaprac;
+
+public class PracJava {
+	
+	private void prac() {
+System.out.println("prac Gituh");
+	}
+	private void prac2() {
+System.out.println("prac bigbucket");
+	}
+	public static void main(String[] args) {
+		
+		PracJava prac=new PracJava();
+		
+		
+	
+	}
+}
